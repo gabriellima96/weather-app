cedf5a3dbbb0df5543b8b34454654c054c19800a
Gabriel Lima <gabriellima.silva96@gmail.com>
2020-11-21 19:00:23 -0300

    feat: criação do migration de city

---
+++ weatherapp-backend/src/main/resources/db/migration/V03__insert_city.sql
@@ -0,0 +1,3 @@
+INSERT INTO city (id, name, country) VALUES (1, 'Maceio', 'Brazil');
+INSERT INTO city (id, name, country) VALUES (2, 'London', 'United Kingdom');
+INSERT INTO city (id, name, country) VALUES (3, 'Blumenau', 'Brazil');
