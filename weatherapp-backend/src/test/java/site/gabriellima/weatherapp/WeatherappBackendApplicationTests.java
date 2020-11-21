4d4ff8a5c01e797f3f6d2a75dbc0e4f9f39d7257
Gabriel Lima <gabriellima.silva96@gmail.com>
2020-11-21 10:47:00 -0300

    feat: initial project

---
+++ weatherapp-backend/src/test/java/site/gabriellima/weatherapp/WeatherappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package site.gabriellima.weatherapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherappBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
